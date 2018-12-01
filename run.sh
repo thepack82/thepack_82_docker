4bd09f12934c01cde1a5f72194daaba8de7ae72a
Omni <omni.maplestory@gmail.com>
2018-12-01 01:14:14 +0000

    Check if containers are already running.

--- run.sh
+++ run.sh
@@ -11,6 +11,12 @@ if [[ ! $NETWORK == "thepack" ]]; then
 fi
 
 # Run containers.
-docker run -d --network thepack --name maplestory-db odinms_mysql
-docker run -d -p 7575:7575 -p 7576:7576 -p 7577:7577 -p 7578:7578 -p 8484:8484 --network thepack --name maplestory odinms
+DB_CONTAINER=`docker container ps | grep -w odinms_mysql | awk {'print $2'}`
+if [[ ! $DB_CONTAINER == "odinms_mysql" ]]; then
+	docker run -d --network thepack --name maplestory-db odinms_mysql
+fi
 
+MS_CONTAINER=`docker container ps | grep -w odinms | awk {'print $2'}`
+if [[ ! $MS_CONTAINER == "odinms" ]]; then
+	docker run -d -p 7575:7575 -p 7576:7576 -p 7577:7577 -p 7578:7578 -p 8484:8484 --network thepack --name maplestory odinms
+fi
