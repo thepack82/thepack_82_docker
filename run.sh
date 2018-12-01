f8cebf5af2790401f710d01e449514582aa7ed44
Omni <omni.maplestory@gmail.com>
2018-12-01 00:04:03 +0000

    Variable not needed.

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@
 docker build -t odinms_mysql odinms_mysql
 docker build -t odinms odinms
 
-ODINMS_MYSQL_CONTAINER_ID=`docker run -d --name odinms_mysql odinms_mysql`
-
+docker run -d --name odinms_mysql odinms_mysql
 docker run -d -p 7575:7575 -p 7576:7576 -p 7577:7577 -p 7578:7578 -p 8484:8484 --name odinms odinms
 
