ef3b8d5f018dff94998d422c39f8d4dee894f727
Omni <omni.maplestory@gmail.com>
2018-12-01 00:22:42 +0000

    Added private network bridge.

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@
 docker build -t odinms_mysql odinms_mysql
 docker build -t odinms odinms
 
-docker run -d --name odinms_mysql odinms_mysql
-docker run -d -p 7575:7575 -p 7576:7576 -p 7577:7577 -p 7578:7578 -p 8484:8484 --name odinms odinms
+docker network create thepack
+
+docker run -d --network thepack --name maplestory-db odinms_mysql
+docker run -d -p 7575:7575 -p 7576:7576 -p 7577:7577 -p 7578:7578 -p 8484:8484 --network thepack --name maplestory odinms
 
